7f1ea1ace813e613e0d05ef229680328665be044
xxl <3161932956@qq.com>
2023-12-16 22:32:52 +0800

    finish the relu and config it

--- src/main/scala/Eyeriss/Buffer/ChipDRAM.scala
+++ src/main/scala/Eyeriss/Buffer/ChipDRAM.scala
@@ -1,7 +1,67 @@
 package Eyeriss.Buffer
 
-/* using the sdram as the off-Chip DRAM */
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba4.axi._
 
-class ChipDRAM {
+trait Arch
+object ASIC extends Arch
+object Memory extends Arch
 
+
+case class DRAMConfig(
+                     DRAMSize:BigInt = 128 MiB,
+                     arch: Arch = Memory,
+                     dataWidth:Int = 64,
+                     ) {
+  def addrWidth = log2Up(DRAMSize / 8)
+  def totalBytes = DRAMSize / 8
+
+  def getAxi4Config = Axi4Config(addressWidth = addrWidth,dataWidth = dataWidth,useId = false,useLock = false,useQos = false,
+    useResp = false,useAllStrb = false,useProt = false,useCache = false)
+}
+
+/* only fetch the DRAM data bits */
+case class Rsp(c:DRAMConfig) extends Bundle with IMasterSlave {
+  val data = Flow (Bits(c.dataWidth bits))
+  override def asMaster(): Unit = {
+    master(data)
+  }
+}
+
+class ChipDRAM(c:DRAMConfig) extends Component {
+
+  val io = new Bundle{
+    val busPort = slave (Axi4(DRAMConfig().getAxi4Config))
+    val rsp = master (Rsp(c))
+  }
+  noIoPrefix()
+
+  val DRAM = Mem(Bits(8 bits),c.addrWidth)
+
+  val rspAddr = RegNextWhen(io.busPort.ar.addr,fire)
+
+  val fire = RegInit(False).setWhen(io.busPort.ar.fire).clearWhen(io.rsp.data.valid)
+  //val burstCounter = Counter(0,)
+
+  val rspData = DRAM.readAsync(rspAddr)
+
+  io.busPort.ar.ready := True
+  io.busPort.ar.setBurstINCR()
+  io.busPort.ar.len := c.dataWidth / 8 - 1
+
+  io.busPort.r.valid := RegNext(io.busPort.ar.fire)
+  io.busPort.r.payload.data := rspData
+  io.busPort.r.last := True
+
+  io.busPort.aw.ready := True
+
+  io.rsp.data.valid := io.busPort.r.valid
+  io.rsp.data.payload := io.busPort.r.payload.data
+}
+
+/* using the chip dram with the axi4 bus port */
+
+object ChipDRAM extends App{
+  SpinalVerilog(new ChipDRAM(DRAMConfig()))
 }
--- src/main/scala/Eyeriss/PE/Relu.scala
+++ src/main/scala/Eyeriss/PE/Relu.scala
@@ -2,33 +2,43 @@ package Eyeriss.PE
 import spinal.core._
 import spinal.lib._
 import Eyeriss._
-import spinal.lib.sim.{StreamDriver, StreamMonitor}
+import spinal.lib.sim.{StreamDriver, StreamMonitor, StreamReadyRandomizer}
 
-class Relu(p:EyerissParameters) extends Component {
+case class ReluConfig(ReluVecNum:Int = 1)
+
+class Relu(p:EyerissParameters,c:ReluConfig) extends Component {
 
   require(p.DataType == SInt16)
 
   val io = new Bundle{
-    val dataIn = slave Stream(SInt(p.DataWidth bits))
-    val act = master Stream(Bits(p.DataWidth bits))
+    val dataIn = slave Stream(Vec(SInt(p.DataWidth bits),c.ReluVecNum))
+    val act = master Stream(Vec(Bits(p.DataWidth bits),c.ReluVecNum))
   }
   /* no pipeline and only trans use the relu unit */
   // io.act.translateFrom(io.dataIn)((to,from) => to := Mux(from > 0,from.asBits,B(0,p.DataWidth bits)))
-
+  noIoPrefix()
   io.act.arbitrationFrom(io.dataIn)
-  io.act.payload := Mux(io.dataIn.payload > 0,io.dataIn.payload.asBits,B(0,p.DataWidth bits))
+
+  (0 until c.ReluVecNum).map{
+    idx =>
+      io.act.payload(idx) := Mux(io.dataIn.payload(idx) > 0,io.dataIn.payload(idx).asBits,B(0,p.DataWidth bits))
+  }
+}
+
+object GenRtl extends App{
+  SpinalVerilog(new Relu(EyerissParameters(),ReluConfig(32)))
 }
 
 object Relu extends App{
   import spinal.core.sim._
 
-  SpinalSimConfig().workspacePath("simulation").compile{
-    val dut = new Relu(EyerissParameters())
+  SpinalSimConfig().workspacePath("simulation").withFstWave.compile{
+    val dut = new Relu(EyerissParameters(),ReluConfig())
     dut
   }.doSimUntilVoid{
     dut =>
       dut.clockDomain.forkStimulus(10)
-
+      StreamReadyRandomizer(dut.io.act,dut.clockDomain)
       val thread = fork{
         var matches = 0
         StreamDriver(dut.io.dataIn,dut.clockDomain){
@@ -36,14 +46,13 @@ object Relu extends App{
             dut.io.dataIn.randomize()
             true
         }
-
         StreamMonitor(dut.io.dataIn,dut.clockDomain){
           payload =>
             matches += 1
-            if(dut.io.dataIn.payload.toBigInt > 0){
-              assert(dut.io.act.payload.toBigInt == payload.toBigInt)
+            if(dut.io.dataIn.payload(0).toBigInt > 0){
+              assert(dut.io.act.payload(0).toBigInt == payload(0).toBigInt)
             }else{
-              assert(dut.io.act.payload.toBigInt == 0)
+              assert(dut.io.act.payload(0).toBigInt == 0)
             }
         }
         dut.clockDomain.waitRisingEdgeWhere(matches == 100)
@@ -52,6 +61,5 @@ object Relu extends App{
       thread.join()
       simSuccess()
   }
-
 
 }
