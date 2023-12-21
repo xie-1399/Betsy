4e1e8050e88ee5b9515b0efa7f571f21b21e868d
xxl <3161932956@qq.com>
2023-12-21 15:21:23 +0800

    update CI

--- src/test/Untils/CI.scala
+++ src/test/Untils/CI.scala
@@ -1,7 +1,8 @@
 package Untils
 
 
-import spinal.core.sim.{SimConfig,SpinalSimConfig}
+import spinal.core.sim._
+import spinal.sim._
 
 /* without the VCS support */
 
@@ -19,9 +20,24 @@ object FST{
     .workspacePath("simulation")
 }
 
+object VCS{
+  /* support for the vcs 2018 */
+  val flags = VCSFlags(
+    compileFlags = List("-kdb"),
+    elaborateFlags = List("-kdb","-LDFLAGS -Wl,--no-as-needed")
+  )
+  val simCfg = SimConfig
+    .withVCS(flags)
+    .withFSDBWave
+    .workspacePath("simulation")
+}
+
+
 object SIMCFG{
   def apply(gtkFirst:Boolean = false): SpinalSimConfig = {
-    if(gtkFirst) CI.simCfg else FST.simCfg
+    sys.env.get("VCS_HOME") match {
+      case Some(_) => VCS.simCfg
+      case None => if(gtkFirst) CI.simCfg else FST.simCfg
   }
 }
 
