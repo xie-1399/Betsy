8d9137af87daf9cdc8fc76af90315ca7f173633f
xxl <3161932956@qq.com>
2024-04-15 00:11:49 +0800

    update the Mac as up down value

--- src/main/scala/Sloan/MAC.scala
+++ src/main/scala/Sloan/MAC.scala
@@ -8,11 +8,14 @@ package Sloan
  ** Todo tobe tested !!! **
  */
 
+import BSUntils.SIMCFG
 import Sloan.Until._
 import spinal.core._
 import spinal.lib._
 
-class MAC[T<: Data with Num[T]](val gen:HardType[T]) extends SloanModule{
+import scala.util.Random
+
+class MAC[T<: Data with Num[T]](val gen:HardType[T],clip:Boolean = true) extends SloanModule{
 
   val io = new Bundle{
     val load =  in Bool()
@@ -32,12 +35,56 @@ class MAC[T<: Data with Num[T]](val gen:HardType[T]) extends SloanModule{
     weight := io.addInput
     io.macOut := weight
   }.otherwise{
-    macOut := upDown(mac(gen.craft(),io.mulInput,weight,io.addInput),gen()).resized
+    val macTemp = if(clip) upDown(mac(gen.craft(),io.mulInput,weight,io.addInput),gen()).resized else mac(gen.craft(),io.mulInput,weight,io.addInput)
+    macOut := macTemp
     io.macOut := macOut
   }
 }
 
-
 object MAC extends App{
-  SpinalSystemVerilog(new MAC(UInt( 8 bits)))
+  import spinal.core.sim._
+  SIMCFG().compile{
+    val dut = new MAC(SInt( 8 bits))
+    dut
+  }.doSimUntilVoid{
+    dut =>
+      dut.clockDomain.forkStimulus(10)
+      /* with clip */
+      def clipValue(width:Int,sign:Boolean,value:Int) = {
+        val max = if(sign) Math.pow(2,width - 1) - 1 else Math.pow(2,width) - 1
+        val min = if(sign) -Math.pow(2,width - 1) else 0
+        val clip = if(value > max){max.toInt}
+        else if(value < min){min.toInt}
+        else{value}
+        clip
+      }
+      def width = 8
+      def sign = true
+      def testCase = 1024
+      val weight = Array.fill(testCase){Random.nextInt(100) - 50}
+      val adds = Array.fill(testCase){Random.nextInt(100) - 50}
+      val activation = Array.fill(testCase){Random.nextInt(100) - 50}
+      dut.io.load #= false
+      dut.io.mulInput #= 0
+      dut.io.addInput #= 0
+      var idx = 0
+      dut.clockDomain.waitSampling()
+      val Mac = fork{
+        // first load
+        while (true){
+          dut.io.load #= true
+          dut.io.addInput #= weight(idx)
+          dut.clockDomain.waitSampling()
+          dut.io.mulInput #= activation(idx)
+          dut.io.addInput #= adds(idx)
+          dut.clockDomain.waitSampling()
+          val refMac = weight(idx) * activation(idx) + adds(idx)
+          assert(dut.io.macOut.toBigInt == clipValue(width, sign, refMac),s"${dut.io.macOut.toBigInt} is not match ${clipValue(width, sign, refMac)}")
+          idx += 1
+          if(idx == testCase){simSuccess()}
+        }
+
+      }
+
+  }
 }
