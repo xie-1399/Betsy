09afde256134020b3b3a1779f07c029e31f85cf1
xxl <3161932956@qq.com>
2024-05-28 20:35:06 +0800

    the stride size has problems

--- src/test/Betsy/StrideHandlerTest.scala
+++ src/test/Betsy/StrideHandlerTest.scala
@@ -6,6 +6,9 @@ import spinal.core.sim._
 import Betsy.Until._
 import BetsyLibs.SIMCFG
 import org.scalatest.funsuite.AnyFunSuite
+import spinal.lib.sim.StreamReadyRandomizer
+import scala.util.Random
+import scala.collection.mutable.ArrayBuffer
 
 case class intoBundle() extends Bundle with Address with Size with Stride with Reverse{
   override val address: UInt = UInt(32 bits)
@@ -31,7 +34,37 @@ class StrideHandlerTest extends AnyFunSuite{
     }.doSimUntilVoid{
       dut =>
         dut.clockDomain.forkStimulus(10)
+        StreamReadyRandomizer(dut.io.output,dut.clockDomain)
+        def getAddress(baseAddress: BigInt, stride: Int, size: Int, reverse: Boolean): ArrayBuffer[BigInt] = {
+          // Todo with the address list size
+          val address = ArrayBuffer[BigInt]()
+          dut.io.into.reverse.randomize()
+          dut.io.into.valid #= false
+          dut.io.into.stride.randomize()
+          dut.io.into.size.randomize()
+          dut.io.into.address.randomize()
+          dut.io.into.payload.randomize()
+          dut.clockDomain.waitSampling()
 
+          dut.io.into.valid #= true
+          dut.io.into.reverse #= reverse
+          dut.io.into.stride #= stride
+          dut.io.into.size #= size
+          dut.io.into.address #= baseAddress
+          dut.clockDomain.waitSamplingWhere {
+            if (dut.io.output.valid.toBoolean && dut.io.output.ready.toBoolean) {
+              address += dut.io.output.address.toBigInt
+            }
+            dut.io.into.ready.toBoolean
+          }
+
+          dut.io.into.valid #= false
+          dut.clockDomain.waitSampling()
+          address
+        }
+
+        val address = getAddress(1000,2,5,false)
+        println(address.mkString(","))
         simSuccess()
     }
   }
