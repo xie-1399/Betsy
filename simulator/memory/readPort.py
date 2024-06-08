51f73df5a8b825473a4455848ab68cd301476c0b
xie-1399 <3161932956@qq.com>
2024-06-08 18:31:16 +0800

    Betsy Simulator Init

---
+++ simulator/memory/readPort.py
@@ -0,0 +1,20 @@
+
+'''
+    Betsy follow the MiT Licence.(c) xxl, All rights reserved
+    Author : xxl    update time：2024.6.8
+'''
+
+
+class readPort:
+
+    def __init__(self, read_Async=False):
+        self.latency = 1  # the Buffer read is Sync like
+        if read_Async:
+            self.latency = 0
+
+    def set_params(self, latency):
+        self.latency = latency
+
+    def get_latency(self):
+        return self.latency
+    
---
+++ simulator/memory/writePort.py
@@ -0,0 +1,10 @@
+'''
+    Betsy follow the MiT Licence.(c) xxl, All rights reserved
+    Author : xxl    update time：2024.6.8
+'''
+
+
+class writePort:
+
+    def __init__(self):
+        self.latency = 0
